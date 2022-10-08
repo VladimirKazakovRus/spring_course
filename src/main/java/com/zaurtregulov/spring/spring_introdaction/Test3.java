0ea36230ddee47486b902e96d59dd13894b862dd
VladimirKazakovRus <ravensolo23@gmail.com>
2022-10-08 20:26:53 +0500

    -ok

--- src/main/java/com/zaurtregulov/spring/spring_introdaction/Test3.java
+++ src/main/java/com/zaurtregulov/spring/spring_introdaction/Test3.java
@@ -1,10 +1,16 @@
 package com.zaurtregulov.spring.spring_introdaction;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test3 {
     public static void main(String[] args) {
-        Pet pet = new Dog();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("myPet",Pet.class);
+        //Pet pet = new Dog();
         Person person = new Person(pet);
         person.CollYourPet();
+
+        context.close();
     }
 
 }
