4dc079b750107b6e1327b8ea520e8870e6ac7532
VladimirKazakovRus <ravensolo23@gmail.com>
2022-10-08 20:57:15 +0500

    set pet+setter

--- src/main/java/com/zaurtregulov/spring/spring_introdaction/Person.java
+++ src/main/java/com/zaurtregulov/spring/spring_introdaction/Person.java
@@ -3,7 +3,16 @@ package com.zaurtregulov.spring.spring_introdaction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        this.pet = pet;
+//    }
+    public Person(){
+        System.out.println("bean is created");
+    }
+
+
+    public void setPet(Pet pet) {
+        System.out.println("class Person: set Pet");
         this.pet = pet;
     }
 
