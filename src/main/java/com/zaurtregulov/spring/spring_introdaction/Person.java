76bfa014325edcab2da925177c050861c1c50612
VladimirKazakovRus <ravensolo23@gmail.com>
2022-10-08 21:10:00 +0500

    new pole + parametr

--- src/main/java/com/zaurtregulov/spring/spring_introdaction/Person.java
+++ src/main/java/com/zaurtregulov/spring/spring_introdaction/Person.java
@@ -2,6 +2,8 @@ package com.zaurtregulov.spring.spring_introdaction;
 
 public class Person {
     private Pet pet;
+    private String surName;
+    private int age;
 
 //    public Person(Pet pet) {
 //        this.pet = pet;
@@ -10,6 +12,21 @@ public class Person {
         System.out.println("bean is created");
     }
 
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public void setPet(Pet pet) {
         System.out.println("class Person: set Pet");
--- src/main/java/com/zaurtregulov/spring/spring_introdaction/Test3.java
+++ src/main/java/com/zaurtregulov/spring/spring_introdaction/Test3.java
@@ -9,7 +9,8 @@ public class Test3 {
         //Pet pet = new Dog();
         Person person = context.getBean("myPerson",Person.class);
         person.CollYourPet();
-
+        System.out.println(person.getAge());
+        System.out.println(person.getSurName());
         context.close();
     }
 
