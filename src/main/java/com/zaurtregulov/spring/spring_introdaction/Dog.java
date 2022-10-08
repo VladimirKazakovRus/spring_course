6bee625fd09062774028e2d38d55165f33c99018
VladimirKazakovRus <ravensolo23@gmail.com>
2022-10-08 22:38:00 +0500

    scope

--- src/main/java/com/zaurtregulov/spring/spring_introdaction/Dog.java
+++ src/main/java/com/zaurtregulov/spring/spring_introdaction/Dog.java
@@ -1,6 +1,19 @@
 package com.zaurtregulov.spring.spring_introdaction;
 
 public class Dog implements Pet{
+    private String name;
+    public Dog(){
+        System.out.println("bean Dog is created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public void sey(){
         System.out.println("Bow-wow");
