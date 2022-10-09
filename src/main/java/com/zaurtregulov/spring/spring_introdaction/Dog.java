42a585dfecbeb52ec48ba794aa2d2eee7725db9f
VladimirKazakovRus <ravensolo23@gmail.com>
2022-10-09 13:09:26 +0500

    init and destroy

--- src/main/java/com/zaurtregulov/spring/spring_introdaction/Dog.java
+++ src/main/java/com/zaurtregulov/spring/spring_introdaction/Dog.java
@@ -1,18 +1,25 @@
 package com.zaurtregulov.spring.spring_introdaction;
 
 public class Dog implements Pet{
-    private String name;
+//    private String name;
     public Dog(){
         System.out.println("bean Dog is created");
     }
 
-    public String getName() {
-        return name;
+    public void init(){
+        System.out.println("class Dog: init metod");
+    }
+    public  void destroy(){
+        System.out.println("class Dog: destroy metod");
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     @Override
     public void sey(){
---
+++ src/main/java/com/zaurtregulov/spring/spring_introdaction/Test5.java
@@ -0,0 +1,17 @@
+package com.zaurtregulov.spring.spring_introdaction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test5 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("ApplicationContext2.xml");
+        Dog myDog = context.getBean("myPet", Dog.class);
+        myDog.sey();
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.sey();
+        context.close();
+
+
+    }
+}
